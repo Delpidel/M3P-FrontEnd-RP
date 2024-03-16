b38241d21600fe98238e47488a337c9698b3fc1f
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-03-16 10:56:59 -0300

    Feat: Iniciando a criação do formulário de medidas do aluno.

--- src/components/AvaliationStep03.vue
+++ src/components/AvaliationStep03.vue
@@ -1,6 +1,71 @@
 <template>
+    <v-app>
+      <v-main>
+        
+        <v-container>
+          <h1 class="text-h5 font-weight-bold ma-2">Medidas do Aluno</h1>
+            <v-card>
+             <v-card-text>
+              <v-row>
+                <v-col>
+                 <v-text-field clearable label="Torax (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Braço Direito (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Braço Esquerdo (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Cintura (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                 <v-text-field clearable label="Antebraço Direito (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Antebraço Esquerdo (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Abdomen (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Quadril (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                 <v-text-field clearable label="Coxa Direita (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Coxa Esquerda (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Panturrilha Direita (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Panturrilha Esquerda (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                 <v-text-field clearable label="Punho (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Bíceps Femoral Direito (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Bíceps Femoral Esquerdo (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+                <v-col>
+                 <v-text-field clearable label="Pescoço (cm)" type="number" variant="outlined"></v-text-field>
+                </v-col>
+              </v-row>
+             </v-card-text>
+            </v-card>
+        </v-container>
+      </v-main>
+    </v-app>
 
-    <h1>
-        implementação da 3º tela de avalição fisica aqui
-    </h1>
 </template>
