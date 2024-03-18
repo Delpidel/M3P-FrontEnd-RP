14760cef5ff962d2b34671c9afc1899dfc3a35f9
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-03-18 06:25:03 -0300

    Feat: Montei o formulário, mas ainda faltam alguns ajustes na estilização. Vou iniciar as validações.

--- src/components/AvaliationStep03.vue
+++ src/components/AvaliationStep03.vue
@@ -1,11 +1,17 @@
 <template>
     <v-app>
+     <v-card color="grey-darken-1" height="210px"  rounded="0" flat>
+      <v-card-item class="d-flex justify-center ml-16">
+        <img class="w-50 ml-16" src="../assets/logo-img/logo (1).png" alt="logo">
+      </v-card-item>
+      
+     </v-card>
       <v-main>
-        
         <v-container>
           <h1 class="text-h5 font-weight-bold ma-2">Medidas do Aluno</h1>
-            <v-card>
+            <v-card >
              <v-card-text>
+              <v-form ref="form">
               <v-row>
                 <v-col>
                  <v-text-field clearable label="Torax (cm)" type="number" variant="outlined"></v-text-field>
@@ -62,6 +68,8 @@
                  <v-text-field clearable label="Pescoço (cm)" type="number" variant="outlined"></v-text-field>
                 </v-col>
               </v-row>
+              <v-btn variant="outlined">Cadastrar</v-btn>
+             </v-form>
              </v-card-text>
             </v-card>
         </v-container>
