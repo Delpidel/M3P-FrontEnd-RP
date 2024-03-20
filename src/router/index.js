d8ad082ad445ba5aa9409230db6753d6c3ffc6ab
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-03-20 19:37:26 -0300

    feat:adiciciona rota de vizualicacao de treino

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import ListStudentWorkout from '@/views/ListStudentWorkout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/listagem-treinos/:id/lista-treino',
+      name: 'listStudenWorkout',
+      component: ListStudentWorkout
     }
   ]
 })
