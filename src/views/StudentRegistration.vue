7236e1e89259e749dd1e87a82e27d782d8ece70f
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2024-03-21 15:19:38 -0300

    feat: adiciona título da página

---
+++ src/views/StudentRegistration.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="d-flex flex-column py-12 px-12">
+            <div class="d-flex">
+                <v-icon size="x-large" class="text-h3" color="amber">mdi-account-multiple-plus-outline</v-icon>
+                <h1 class="font-weight-medium ms-2 mt-1">Cadastro de aluno</h1>
+            </div>
+            <v-progress-linear 
+                color="gre-darken-4" 
+                indeterminate>
+            </v-progress-linear>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
