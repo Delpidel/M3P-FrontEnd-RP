6b02a69ce42cbd9db6b668c2be8660b34c8a10fd
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2024-03-21 16:30:28 -0300

    feat: adiciona formulário de cadastro

--- src/views/StudentRegistration.vue
+++ src/views/StudentRegistration.vue
@@ -10,11 +10,108 @@
                 indeterminate>
             </v-progress-linear>
         </div>
+        <div>
+            <div>
+                <v-form>
+                    <div class="w-25" style="position: relative; border: #212121 3px solid">
+                        <simple-v-camera
+                            :resolution="{ width:400, height: 400}"
+                            ref="camera"
+                            autoplay
+                        ></simple-v-camera>
+                    </div>
+                    <v-text-field
+                        v-model="name"
+                        label="Nome completo"
+                        type="text"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="email"
+                        label="E-mail"
+                        type="email"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="contact"
+                        label="Telefone para contato"
+                        type="number"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="dateBirth"
+                        label="Data de Nascimento"
+                        type="date"
+                        variant="outlined"
+                    />
+
+                    <v-divider class="mb-4"></v-divider>
+
+                    <v-text-field
+                        v-model="cep"
+                        label="CEP"
+                        type="number"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="street"
+                        label="Logradouro"
+                        type="text"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="number"
+                        label="Número"
+                        type="number"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="neighborhood"
+                        label="Bairro"
+                        type="text"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="city"
+                        label="Cidade"
+                        type="text"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="state"
+                        label="Estado"
+                        type="text"
+                        variant="outlined"
+                    />
+
+                    <v-text-field
+                        v-model="complement"
+                        label="Complemento"
+                        type="text"
+                        variant="outlined"
+                    />
+                </v-form>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Camera from 'simple-vue-camera'
 
+    export default {
+        components: {
+            'simple-v-camera': Camera
+        },
+    }
 
 </script>
 
