6e348a0c565aa8b29a62847f2a80fa2f5676f2fa
Guilherme <guiduartesbs@hotmail.com>
2024-03-25 22:40:27 -0300

    feat: add students/meal_plans route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import StudentMealPlans from '@/views/StudentMealPlans.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +16,12 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: Home
-    }
+    },
+    {
+      path: '/students/meal_plans',
+      name: 'studentMealPlans',
+      component: StudentMealPlans
+    },
   ]
 })
 
