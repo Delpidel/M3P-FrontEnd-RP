12f5b3bcd754b122f084b513bffebb6bde710605
joseCdia <josecdia@hotmail.com>
2024-03-27 19:44:59 -0300

    add

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,5 @@
+<template>
+      <h1>DashBoard </h1>
+
+  <img src="https://s2.glbimg.com/dWcTfyFzZxj6dL-I-RK7NZTXebs=/475x315/top/i.glbimg.com/og/ig/infoglobo/f/original/2016/02/17/homer.jpg" width="100px" />
+</template>
