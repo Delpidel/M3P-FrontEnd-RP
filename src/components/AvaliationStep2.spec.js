f003fe18f5383cd900e8f4d4ee09edaa2cad1267
lcsrlm <lucas_rosa-lima@estudante.sesisenai.org.br>
2024-03-31 19:27:23 -0300

    feat: "adiciona testes das imagens padrão"

--- src/components/AvaliationStep2.spec.js
+++ src/components/AvaliationStep2.spec.js
@@ -25,12 +25,64 @@ describe("Testa página de avaliação", () => {
                 plugins: [vuetify]
             }
         })
-
-        await flushPromises()
-        
-        const cards = component.findAll(concatId('card-item'))
-        
+        await flushPromises()       
+        const cards = component.findAll(concatId('card-item'))      
         expect(cards).toHaveLength(4)
     })
 
-})
+    it("Espera-se que as imagens svg sejam carregadas", async () => {
+        const component = mount(AvaliationStep02)
+        await flushPromises()
+        const images = component.findAll('img')
+        expect(images).toBeTruthy()
+    })
+    it("Espera-se que sejam carregadas 4 imagens", async () => {
+        const component = mount(AvaliationStep02)
+        await flushPromises()
+        const images = component.findAll('img')
+        expect(images).toHaveLength(4)
+    })
+    it("Espera-se que as imagens tenham os atributos corretos", async () => {
+        const component = mount(AvaliationStep02)
+        await flushPromises();
+        const images = component.findAll('img')
+        images.forEach((img, index) => {
+        expect(img.attributes('src')).toContain(`../src/assets/avaliation-images/${getOriginalImageName(index)}`)
+        expect(img.attributes('height')).toBe('150')
+        expect(img.attributes('width')).toBe('150')
+        expect(img.attributes('alt')).toBe(getImageAlt(index))
+        })
+        function getOriginalImageName(index) {
+            switch (index) {
+                case 0:
+                    return 'front.svg';
+                case 1:
+                    return 'right.svg';
+                case 2:
+                    return 'back.svg';
+                case 3:
+                    return 'left.svg';
+                default:
+                    return '';
+            }
+        }
+        
+        function getImageAlt(index) {
+            switch (index) {
+                case 0:
+                    return 'Homem virado para frente';
+                case 1:
+                    return 'Homem virado para direita';
+                case 2:
+                    return 'Homem virado para trás';
+                case 3:
+                    return 'Homem virado para esquerda';
+                default:
+                    return '';
+            }
+        }    
+
+    })
+
+
+}) 
