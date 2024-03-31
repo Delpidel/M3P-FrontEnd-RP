026d6c207f602ba9155637a922bfdc30c6c1bd65
Drew <84882983+vdr3w@users.noreply.github.com>
2024-03-30 22:06:10 -0300

    feat: Enhance dashboard with localStorage and API data fetching
    Adicionada a lógica para buscar o nome do usuário no localStorage e as informações de alunos registrados e exercícios cadastrados através da API.

--- src/views/Instructor/InstructorDashboard.vue
+++ src/views/Instructor/InstructorDashboard.vue
@@ -1,40 +1,64 @@
 <template>
   <v-container fluid>
     <div class="container">
-      <v-row class="mb-5">
-        <v-col cols="12">
-          <h1 class="font-weight-bold text-center">Olá, Ususario!</h1>
-          <h2 class="mt-2 font-weight-medium text-center">
-            O sucesso de seus alunos começa com você. Vamos começar!
-          </h2>
+      <v-row class="px-16">
+        <v-col cols="12" md="10" offset-md="1">
+          <v-card class="title-card elevation-10" flat>
+            <v-card-text class="d-flex flex-column align-center">
+              <h1 class="font-weight-bold text-center">Olá, {{ userName }}!</h1>
+              <h2 class="mt-2 font-weight-medium text-center">
+                O sucesso de seus alunos começa com você. Vamos começar!
+              </h2>
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
 
-      <v-row justify="space-around">
-        <v-col cols="12" md="5">
-          <v-card class="user-card elevation-2">
-            <v-card-text>
+      <v-row justify="center" class="px-16" >
+        <v-col cols="12" md="5" class="mx-2">
+          <v-card class="user-card elevation-10">
+            <v-card-text class="d-flex flex-column justify-end">
+              <img
+              src="../../assets//instructor/dashboard/mulhercard.png"
+              alt="imagem vetorizada"
+              class="womanImage hidden-sm-and-down"
+              
+                />
               <div class="text-center">
-                <img src="../../assets/exercises.svg" class="card-image" alt="Imagem" />
-                <div class="text-h2 my-3">{{ registeredStudents }}</div>
-                <div class="text-h5">Alunos Registrados</div>
-                <v-btn color="black" class="mt-2">
-                  <span style="color: #ffc107">VER</span>
+                <div class="text-h2">{{ registeredStudents }}</div>
+                <div class="text-h5 font-weight-bold">ALUNOS REGISTRADOS</div>
+                <v-btn
+                size="large"
+                  variant="elevated"
+                  color="grey-darken-4 text-amber"
+                  class="font-weight-bold mt-2"
+                >
+                  VER
                 </v-btn>
               </div>
             </v-card-text>
           </v-card>
         </v-col>
 
-        <v-col cols="12" md="5">
-          <v-card class="user-card elevation-2">
-            <v-card-text>
+        <v-col cols="12" md="5" class="mx-2">
+          <v-card class="user-card elevation-10">
+            <v-card-text class="d-flex flex-column justify-end">
+              <img
+              src="../../assets//instructor/dashboard/homemcard.png"
+              alt="imagem vetorizada"
+              class="womanImage hidden-sm-and-down"
+              
+                />
               <div class="text-center">
-                <img src="../../assets/exercises.svg" class="card-image" alt="Imagem" />
-                <div class="text-h2 my-3">{{ registeredExercises }}</div>
-                <div class="text-h5">Exercícios Cadastrados</div>
-                <v-btn color="black" class="mt-2">
-                  <span style="color: #ffc107">VER</span>
+                <div class="text-h2">{{ registeredExercises }}</div>
+                <div class="text-h5 font-weight-bold">EXERCÍCIOS CADASTRADOS</div>
+                <v-btn
+                size="large"
+                  variant="elevated"
+                  color="grey-darken-4 text-amber"
+                  class="font-weight-bold mt-2"
+                >
+                  VER
                 </v-btn>
               </div>
             </v-card-text>
@@ -48,42 +72,75 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import { useDisplay } from 'vuetify';
 
 const registeredStudents = ref(0);
 const registeredExercises = ref(0);
+const userName = ref('Usuário'); // Inicializa com um valor padrão
+
+const { smAndDown, mdAndDown, lgAndDown } = useDisplay();
 
 onMounted(async () => {
-  const token = localStorage.getItem('@token');
-  try {
-    const response = await axios.get('http://localhost:8000/api/dashboard/instrutor', {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+  // Busca o nome do usuário do localStorage e atribui à variável reativa userName
+  userName.value = localStorage.getItem('@name') || 'Usuário';
 
-    registeredStudents.value = response.data.registered_students;
-    registeredExercises.value = response.data.registered_exercises;
-  } catch (error) {
-    console.error('Falha ao buscar dados do instrutor:', error);
+  const token = localStorage.getItem('@token');
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    try {
+      const response = await axios.get('http://localhost:8000/api/dashboard/instrutor', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      registeredStudents.value = response.data.registered_students;
+      registeredExercises.value = response.data.registered_exercises;
+      // Mantém a chamada API para os números dos cards, sem alterar essa parte
+    } catch (error) {
+      console.error('Falha ao buscar dados do instrutor:', error);
+    }
   }
 });
+
 </script>
 
 <style scoped>
 .container {
-  max-width: 100%;
+  width: 100%;
+  height: 100%;
+}
+
+.title-card {
+  border-radius: 1.5rem;
+  background: rgb(255, 212, 80);
+  background: linear-gradient(160deg, rgba(255, 212, 80, 1) 0%, rgba(222, 167, 0, 1) 100%);
+  box-shadow:
+    12px 16px 28px -2px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)),
+    0px 2px 2px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)),
+    0px 2px 4px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12)),
+    inset 1px 1px 0px 0px var(--v-shadow-key-penumbra-opacity, rgba(255, 255, 255, 0.8));
 }
 
 .user-card {
-  background: linear-gradient(to right, #ffc107, #ffa000);
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  max-width: 500px;
-  margin: auto;
+  border-radius: 1.5rem;
+  background: rgb(255, 212, 80);
+  background: linear-gradient(160deg, rgba(255, 212, 80, 1) 0%, rgba(222, 167, 0, 1) 100%);
+  box-shadow:
+    12px 16px 28px -2px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)),
+    0px 2px 2px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)),
+    0px 2px 4px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12)),
+    inset 1px 1px 0px 0px var(--v-shadow-key-penumbra-opacity, rgba(255, 255, 255, 0.8));
+    min-height: 550px; /* Defina uma altura mínima para garantir que ambos os cards comecem com a mesma altura */
+  position: relative;
+  overflow: visible;
 }
+
 
 .card-image {
   max-width: 100%;
   height: auto;
 }
+
+
+
 </style>
