9999cf1ff663fd3b4be79ae4fc8724daa5d737f5
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-01 13:20:54 -0300

    feat:adiciona botoes e metodo de delecao. Adiciona classe de estilos.

--- src/views/InstructorViews/ListStudentWorkout.vue
+++ src/views/InstructorViews/ListStudentWorkout.vue
@@ -4,44 +4,67 @@
       <v-card-title class="d-flex align-center justify-space-between">
         <div class="d-flex align-center">
           <v-icon class="mr-2">mdi mdi-account-supervisor</v-icon>
-          <h1 class="py-6">Treinos {{ studentName }}</h1>
+          <h1 class="py-12">Treinos {{ studentName }}</h1>
         </div>
         <img src="@/assets/logo.svg" alt="Logo" style="height: 80px;">
       </v-card-title>
     </v-card>
     <v-toolbar color="#757575">
-      <v-btn class="ml-auto" type="submit" variant="elevated font-weight-bold" size="large" @click="newWorkout">
+      <v-btn class="ml-auto"
+       type="submit" 
+       variant="elevated font-weight-bold" 
+       size="large" 
+       @click="newWorkout">
         Novo Treino
       </v-btn>
 
       <template v-slot:extension>
-        <v-tabs v-model="days" centered>
-          <v-tab v-for="(day, index) in daysOfWeek" :key="index" :value="day.value" :prepend-icon="day.icon">{{ day.label }}</v-tab>
+        <v-tabs v-model="days" centered color="#424242">
+          <v-tab v-for="(day, index) in daysOfWeek" 
+          :key="index" :value="day.value" 
+          :prepend-icon="day.icon">{{
+            day.label }}</v-tab>
         </v-tabs>
       </template>
     </v-toolbar>
     <v-window v-model="days">
-      <v-window-item v-for="(day, index) in daysOfWeek" :key="index" :value="day.value">
-        <v-table>
-          <thead>
-            <tr class="text-left">
-              <th>Exercício</th>
-              <th>Peso</th>
-              <th>Repetições</th>
-              <th>Pausa (s)</th>
-              <th>Tempo</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="workout in workoutsList" :key="workout.id">
-              <td>{{ workout.exercise.description }}</td>
-              <td>{{ workout.weight }} kg</td>
-              <td>{{ workout.repetitions }}</td>
-              <td>{{ workout.break_time }}</td>
-              <td>{{ workout.time }}</td>
-            </tr>
-          </tbody>
-        </v-table>
+      <v-window-item 
+      v-for="(day, index) in daysOfWeek" 
+      :key="index" 
+      :value="day.value">
+          <v-table v-if="workoutsList.length > 0">
+            <thead>
+              <tr>
+                <th class="head-descriptions">Exercício</th>
+                <th class="head-descriptions">Peso</th>
+                <th class="head-descriptions">Repetições</th>
+                <th class="head-descriptions">Pausa (s)</th>
+                <th class="head-descriptions">Tempo</th>
+                <th class="head-descriptions">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="workout in workoutsList" :key="workout.id">
+                <td>{{ workout.exercise.description }}</td>
+                <td>{{ workout.weight }} kg</td>
+                <td>{{ workout.repetitions }}</td>
+                <td>{{ workout.break_time }}</td>
+                <td>{{ workout.time }}</td>
+                <td>
+                  <v-btn @click="updateWorkout">
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                  <v-btn @click="deleteWorkout(workout.id)">
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+          <div v-else 
+          class="text-center py-4">
+          Não há treinos para este dia.
+        </div>
       </v-window-item>
     </v-window>
   </div>
@@ -91,7 +114,7 @@ export default {
           const workouts = response.data.workouts;
           const currentDay = this.days.toUpperCase();
           if (workouts[currentDay]) {
-            this.workoutsList = Object.values(workouts[currentDay]); 
+            this.workoutsList = Object.values(workouts[currentDay]);
           } else {
             this.workoutsList = [];
           }
@@ -102,7 +125,32 @@ export default {
     },
     newWorkout() {
       this.$router.push('/newWorkout/:id')
+    },
+    updateWorkout() {
+      this.$router.push('/newWorkout/:id')
+    },
+    deleteWorkout(workoutId){
+      axios.delete(`http://127.0.0.1:8000/api/workouts/${workoutId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('@token')}`
+        }
+      }).then(() => {
+        alert("Treino excluído!")
+          // Recarregar os treinos após a exclusão
+        this.loadWorkout();
+      }).catch(() => {
+        alert('Erro ao excluir o treino.');
+      });
     }
   }
 }
 </script>
+<style scoped>
+.head-descriptions {
+  background-color: #424242;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 1.1rem;
+}
+
+</style>
