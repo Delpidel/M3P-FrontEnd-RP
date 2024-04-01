0eb8debe7bb8a8c4209ed1a595d2faa4950e250a
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-04-01 19:28:03 -0300

    feat: inclui a tabela e a linha de pesquisa a listagem dos estudante

--- src/views/RecepcionistaViews/ListStudents.vue
+++ src/views/RecepcionistaViews/ListStudents.vue
@@ -6,16 +6,62 @@
         >Novo estudante</v-btn
       >
     </div>
-    <form @submit.prevent="handleSearch"></form>
+    <form @submit.prevent="handleSearch">
+      <v-row>
+        <v-col cols="12" class="my-2">
+          <v-text-field
+            placeholder="Pesquise por uma informação do estudante"
+            variant="outlined"
+            v-model="text"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </form>
+    <v-data-table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Cpf</th>
+          <th>Email</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <t-body>
+        <tr v-for="student in students" :key="student.id">
+          <td>{{ student.name }}</td>
+          <td>{{ student.cpf }}</td>
+          <td>{{ student.email }}</td>
+        </tr>
+      </t-body>
+    </v-data-table>
   </v-container>
 </template>
 
 <script>
+import StudentService from '../../services/StudentService'
 export default {
-  data() {},
+  data() {
+    return {
+      text: '',
+      students: []
+    }
+  },
 
   methods: {
-    handleSearch() {}
+    handleSearch() {
+      this.getStudents()
+    },
+    getStudents() {
+      StudentService.gettAllStudents(this.text)
+        .then((data) => {
+          this.student = data
+        })
+        .catch(() => alert('Houve um erro ao retornar os estudantes'))
+    }
+  },
+
+  mounted() {
+    this.getStudents()
   }
 }
 </script>
