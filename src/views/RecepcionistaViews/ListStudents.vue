edbf86ac65ecbbb328d35d40a15056a7728c444d
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-04-01 13:06:53 -0300

    feat: começa o desenvolvimento da pagina de listagem de estudante

--- src/views/RecepcionistaViews/ListStudents.vue
+++ src/views/RecepcionistaViews/ListStudents.vue
@@ -1,3 +1,21 @@
 <template>
-  <div></div>
+  <v-container>
+    <div class="d-flex justify-space-between">
+      <h1 class="py-12">Listagem de estudantes</h1>
+      <v-btn type="submit" variant="elevated" color="grey-darken-4" text="amber"
+        >Novo estudante</v-btn
+      >
+    </div>
+    <form @submit.prevent="handleSearch"></form>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {},
+
+  methods: {
+    handleSearch() {}
+  }
+}
+</script>
