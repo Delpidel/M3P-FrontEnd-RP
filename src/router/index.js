9ba525b0203e601179c9e30811372bc1eb72b659
joseCdia <josecdia@hotmail.com>
2024-04-04 20:54:39 -0300

    Movendo arquivos para a pasta instructorViews

--- src/router/index.js
+++ src/router/index.js
@@ -2,29 +2,29 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ListStudentWorkout from '@/views/InstructorViews/ListStudentWorkout.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
-import Exercises from '../views/exercise/ExercisesPage.vue'
+import Exercises from '../views/InstructorViews/ExercisesPage.vue'
 
 import Exemplo from '../views/PaginaExemplo.vue'
 
 import CreateStudentWorkout from '@/views/InstructorViews/CreateStudentWorkout.vue'
 
-import ExercisesList from '../views/Exercises/ExercisesList.vue'
+// import ExercisesList from '../views/Exercises/ExercisesList.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes:[
+  routes: [
     {
       path: '/',
       name: 'Login',
       component: Login
     },
-   /* {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard
-    },
-*/
+     {
+       path: '/dashboard',
+       name: 'Dashboard',
+       component: Dashboard
+     },
+ 
     //perfil usuário
     {
       path: '/users',
@@ -49,12 +49,12 @@ const router = createRouter({
       component: Exemplo
     },
 
-    //perfil instrutor
+    // perfil instrutor
     {
       path: '/exercises',
 
       name: 'Exercises',
-      component: ExercisesList  
+      component: Exercises
     },
     {
       path: '/instructor/students',
