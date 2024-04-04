30cab7c5e9d00c9f66d68e15a6c8de2c4ce91f96
joseCdia <josecdia@hotmail.com>
2024-04-04 00:22:07 -0300

    Adicionando lista de exercicios na classe ExercisesPage e adicionando teste de carregar lista de exercicios na tela

--- src/services/ExerciseService.js
+++ src/services/ExerciseService.js
@@ -1,18 +1,29 @@
 import api from './api'
 
 class ExerciseService {
-    
+
     async createExercise(body) {
-        
+
         const response = await api.post('exercises', body, {
             headers: {
-                
+
             token:localStorage.getItem('@token'),
                 'Content-Type': 'multipart/form-data'
             }
         })
         return response.data
     }
+
+    async getAllExercises(page = '') {
+        const response = await api.get(`/exercises?page=${page}`,{
+            headers:{
+                token:localStorage.getItem('@token'),
+                'Content-Type':'multipart/form-data'
+            }
+        })
+        return response.data
+    }
+
 }
 
 export default new ExerciseService()
