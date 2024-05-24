be941741ffb2bd8a794cabf84340d1920338816e
Helena Delpizzo <hdelpizzo@gmail.com>
2024-05-24 14:18:44 -0300

    refactor: dashboard

--- src/components/Dashboard/DashboardAdmin.vue
+++ src/components/Dashboard/DashboardAdmin.vue
@@ -16,39 +16,8 @@
         </v-col>
       </v-row>
       <v-row justify="center">
-        <v-col cols="12" md="5" :class="mdAndDown ? 'my-2' : 'my-0'">
-          <v-card class="user-card elevation-10" style="cursor: pointer">
-            <v-card-text class="d-flex flex-column justify-end">
-              <img
-                src="@/assets/Dashboard/equipamentos-exercicios.svg"
-                alt="Imagem com equipamentos que remetem a exercicios"
-                class="card-image"
-              />
-              <div class="text-center">
-                <v-card-title>Exercícios Registrados</v-card-title>
-                <v-card-text class="text-h3 font-weight-bold">{{ registeredExercises }}</v-card-text>
-                <span class="text-h3 font-weight-bold" v-if="!showExercises"></span>
-                <v-list class="user-card" v-else>
-                  <v-list-item v-for="exercise in exercises" :key="exercise.id">
-                      <v-list-item-title>{{ exercise.description }}</v-list-item-title>
-                  </v-list-item>
-                </v-list>
-                <v-btn
-                  @click="toggleExercises"
-                  append-icon="mdi-dumbbell"
-                  size="large"
-                  variant="elevated"
-                  color="grey-darken-4 text-amber"
-                  class="font-weight-bold my-6"
-                  :class="smAndDown ? 'my-custom-small-button-class' : 'my-custom-large-button-class'"
-                >
-                  Visualizar
-                </v-btn>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="12" md="5" :class="mdAndDown ? 'my-2' : 'my-0'">
+ 
+        <v-col cols="12" md="10" :class="mdAndDown ? 'my-2' : 'my-0'">
           <v-card class="user-card elevation-10" style="cursor: pointer">
             <v-card-text class="d-flex flex-column justify-end">
             <img
@@ -60,21 +29,10 @@
               <v-card-title>Usuários Cadastrados</v-card-title>
               <v-card-text class="text-h3 font-weight-bold">{{ totalUsers }}</v-card-text>
               <span class="text-h3 font-weight-bold" v-if="!showUsers"></span>
-              <v-list class="user-card" v-else>
+              <v-list class="user-card-list" v-else>
                 <v-list-item v-for="(count, profile) in profiles" :key="profile">
                     <v-list-item-title>{{ profile }}: {{ count }}</v-list-item-title>
                 </v-list-item>
-                <v-btn
-                  to="/users"
-                  append-icon="mdi-account-circle"
-                  size="large"
-                  variant="elevated"
-                  color="grey-darken-4 text-amber"
-                  class="font-weight-bold my-6 mx-2"
-                  :class="smAndDown ? 'my-custom-small-button-class' : 'my-custom-large-button-class'"
-                >
-                  Listagem completa
-                </v-btn>
                 
                 <v-btn
                   @click="navigateToCreateUser"
@@ -123,8 +81,6 @@ export default {
       profileName: localStorage.getItem('@name'),
       registeredExercises: 0,
       profiles: {},
-      exercises: [],
-      showExercises: false,
       showUsers: false
     }
   },
@@ -134,16 +90,12 @@ export default {
   methods: {
     async fetchDashboardData() {
       try {
-        const { registered_exercises, profiles, exercises } = await AuthenticationService.fetchDashboardData()
-        this.registeredExercises = registered_exercises
+        const { profiles} = await AuthenticationService.fetchDashboardData()
         this.profiles = profiles
-        this.exercises = exercises
+
       } catch (error) {
         console.error('Erro ao buscar dados do painel de administração:', error)
       }
-    },
-    toggleExercises() {
-      this.showExercises = !this.showExercises
     },
     toggleUsers() {
       this.showUsers = !this.showUsers
@@ -182,9 +134,19 @@ export default {
   min-height: auto;
 }
 
+.user-card-list {
+  border-radius: 1rem;
+  box-shadow:
+    12px 16px 28px -2px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)),
+    0px 2px 2px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)),
+    0px 2px 4px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12)),
+    inset 1px 1px 0px 0px var(--v-shadow-key-penumbra-opacity, rgba(255, 255, 255, 0.8));
+}
+
 .card-image {
-  max-width: 100%;
+  max-width: 45%;
   height: auto;
+  align-self: center;
 }
 
 @media (max-width: 768px) {
